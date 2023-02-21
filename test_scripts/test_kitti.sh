cc34c861cb71aec48ebdaa0cfe2acfa030a444ec
vineethak <vineetha.knd92@gmail.com>
2023-02-21 15:10:52 -0500

    Add few local results and testing scripts

---
+++ test_scripts/test_kitti.sh
@@ -0,0 +1,3 @@
+python tools/test.py configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class.py \
+    checkpoints/hv_pointpillars_secfpn_6x8_160e_kitti-3d-3class_20220301_150306-37dc2420.pth --eval mAP \
+    
